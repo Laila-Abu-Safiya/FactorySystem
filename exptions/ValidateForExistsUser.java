28833cd1f6062707b6e2a73fb8d89039b7951a06
Laila AbuSafiya <labus1249@gmail.com>
2023-08-01 15:53:22 +0300

    delete files form machine_part branch

--- exptions/ValidateForExistsUser.java
+++
@@ -1,7 +0,0 @@
-package com.example.spring_boot.exptions;
-
-public class ValidateForExistsUser extends Exception {
-    public ValidateForExistsUser(String message) {
-        super(message);
-    }
-}
