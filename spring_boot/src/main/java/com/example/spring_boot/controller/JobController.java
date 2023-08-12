a1ac1a1edfa671fa8cb8dacfefb9a76ff7f8d712
Laila AbuSafiya <100162487+Laila-Abu-Safiya@users.noreply.github.com>
2023-08-13 00:02:12 +0300

    Update JobController.java

--- spring_boot/src/main/java/com/example/spring_boot/controller/JobController.java
+++ spring_boot/src/main/java/com/example/spring_boot/controller/JobController.java
@@ -1,19 +1,26 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
 package com.example.spring_boot.controller;
 
 import com.example.spring_boot.Entity.Job;
-import com.example.spring_boot.Entity.JobMachineRequest;
 import com.example.spring_boot.exptions.ValidateForExistsUser;
 import com.example.spring_boot.exptions.ValidateUSerOwner;
 import com.example.spring_boot.service.JobService;
 import com.example.spring_boot.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class JobController {
-
     private final JobService jobService;
     private final UserService userService;
 
@@ -23,52 +30,50 @@ public class JobController {
         this.userService = userService;
     }
 
-    @PostMapping(path = "user/{userId}/job/task")
+    @PostMapping(
+        path = {"user/{userId}/job/task"}
+    )
     public Job addNewMachine(@RequestBody Job job, @PathVariable("userId") int userId) throws Exception {
-        boolean userExists = userService.checkIfUserExists(userId);
-
-        checkIfUserExists(userExists);
-        return jobService.addNewJob(job);
-
+        return this.jobService.addNewJob(job);
     }
 
-    @DeleteMapping(path = "tenant/{userId}/job/task")
+    @DeleteMapping(
+        path = {"tenant/{userId}/job/task"}
+    )
     public void deleteTask(@PathVariable("userId") int userId, @RequestBody int taskId) throws Exception {
-        boolean userExists = userService.checkIfUserExists(userId);
-        boolean isUserOwner = jobService.checkIfUserIsOwner(userId, taskId);
-
-        checkIfUserExists(userExists);
-        checkIfTheUserOwner(isUserOwner);
-
-        jobService.deleteTask(taskId);
-
+        boolean userExists = this.userService.checkIfUserExists(userId);
+        this.checkIfUserExists(userExists);
+        this.jobService.deleteTask(taskId);
     }
 
-    @GetMapping(path = "tenant/{userId}/job/task/{taskId}")
-    public Optional<Job> getTaskById(@PathVariable("taskId") int taskId) throws Exception {
-        boolean exists = jobService.CheckIfTaskExists(taskId);
-
+    @GetMapping(
+        path = {"tenant/{userId}/job/task/{taskId}"}
+    )
+    public Optional<Job> getTaskById(@PathVariable("userId") int userId, @PathVariable("taskId") int taskId) throws Exception {
+        boolean userExists = this.userService.checkIfUserExists(userId);
+        this.checkIfUserExists(userExists);
+        boolean exists = this.jobService.CheckIfTaskExists(taskId);
         if (exists) {
-            Optional<Job> task = jobService.getTaskById(taskId);
+            Optional<Job> task = this.jobService.getTaskById(taskId);
             return task;
         } else {
             throw new Exception("Task is not exists");
         }
     }
 
-    @PostMapping(path = "tenant/{userId}/job/task/{taskId}")
+    @PostMapping(
+        path = {"tenant/{userId}/job/task/{taskId}"}
+    )
     public Job resubmitSpecificTask(@RequestBody Job job, @PathVariable("userId") int userId, @PathVariable("taskId") int taskId) throws Exception {
-        boolean userExists = userService.checkIfUserExists(userId);
-
-        checkIfUserExists(userExists);
-
-        Optional<Job> task = jobService.getTaskById(taskId);
-        if (task.get().getStatus().equals("pending")) {
+        boolean userExists = this.userService.checkIfUserExists(userId);
+        this.checkIfUserExists(userExists);
+        Optional<Job> task = this.jobService.getTaskById(taskId);
+        if (((Job)task.get()).getStatus().equals("pending")) {
             throw new Exception("This task is not in final state");
+        } else {
+            job.setTaskid(taskId);
+            return this.jobService.addNewJob(job);
         }
-        job.setTaskid(taskId);
-        return jobService.addNewJob(job);
-
     }
 
     public void checkIfUserExists(boolean exists) throws ValidateForExistsUser {
